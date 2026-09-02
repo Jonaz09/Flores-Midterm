5c3b6c57e1ee6d62cd61344e02326269d2f57597
Naz <jonaz09flores@gmail.com>
2026-09-02 17:19:43 +0800

    Initial Vite project setup

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import Button from '@mui/material/Button';
+import { Typography } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
+function App() {
+  return (
+  <div>
+   <h1>hello</h1>
+   <Button variant="contained">
+    My first button
+   </Button>
+
+   <Button variant="outlined">
+    My first button
+   </Button>
+
+   <Button variant="text">
+    My first button
+   </Button>
+    
+  </div>
+  );
+}
+
+export default App;
+
+
+
